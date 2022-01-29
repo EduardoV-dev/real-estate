4da7f1525114f6c710feb20ab200ae04dbc90a53
Eduardo Varela <eduardovarela139@gmail.com>
2022-01-29 17:06:48 -0600

    initial configs done

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+const RULES = {
+  ON: 'on',
+  OFF: 'off',
+  WARN: 'warn',
+  ERR: 'error',
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': RULES.WARN,
+    'react/prop-types': RULES.OFF,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+};
